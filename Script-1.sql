c05cdb878cd6b7718282daf168e4635ce9ff2aae
Esther <esther.camilo@gntech.med.br>
2024-10-14 11:40:54 -0300

    exercíos 14-out

---
+++ Script-1.sql
@@ -0,0 +1,52 @@
+-- Exercício 1: Criação de Tabela e Inserção de Dados
+
+-- Crie uma tabela chamada 'alunos' com as colunas:
+-- id (inteiro, chave primária, auto-incremento)
+-- nome (texto, não pode ser nulo)
+-- idade (inteiro)
+-- cidade (texto)
+
+
+-- Insira pelo menos 3 alunos na tabela 'alunos'.
+
+
+
+
+
+
+
+
+-- Exercício 2: Seleção Simples
+
+-- Escreva uma query para selecionar todos os registros da tabela 'alunos'.
+
+
+
+
+
+
+
+
+-- Exercício 3: Filtragem de Dados
+
+-- Escreva uma query para selecionar os alunos com idade maior que 20 anos.
+
+
+
+
+
+
+
+-- Exercício 4: Atualização de Dados
+
+-- Escreva uma query para atualizar a cidade do aluno 'Pedro' para 'Curitiba'.
+
+
+
+
+
+
+
+-- Exercício 5: Exclusão de Registros
+
+-- Escreva uma query para excluir o aluno com id igual a 1.
